0b48ff087ec2eb9dcad53d80e1f4d94dde016b6e
asifdotpy <asifdotpy@gmail.com>
2022-04-05 15:16:49 +0600

    basic template

---
+++ src/components/QuickStart.jsx
@@ -0,0 +1,66 @@
+import { Typography } from "antd";
+import React, { useMemo } from "react";
+import { useMoralis } from "react-moralis";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const { Text } = Typography;
+
+const styles = {
+  title: {
+    fontSize: "20px",
+    fontWeight: "700",
+  },
+  text: {
+    fontSize: "16px",
+  },
+  card: {
+    boxShadow: "0 0.5rem 1.2rem rgb(189 197 209 / 20%)",
+    border: "1px solid #e7eaf3",
+    borderRadius: "0.5rem",
+  },
+  timeline: {
+    marginBottom: "-45px",
+  },
+};
+
+export default function QuickStart({ isServerInfo }) {
+  const { Moralis } = useMoralis();
+
+  const isInchDex = useMemo(
+    () => (Moralis.Plugins?.oneInch ? true : false),
+    [Moralis.Plugins?.oneInch],
+  );
+
+  return (
+
+    <div className="parent-stake-tab">
+      <div className="stake-tab">
+        <div className="card text-center">
+          <div className="card-header">
+            <ul className="nav nav-pills card-header-pills">
+              <li className="nav-item">
+                <a className="nav-link active" href="#">Stake</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Unstake</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+              </li>
+            </ul>
+          </div>
+          <div className="card-body">
+            <h5 className="card-title" style={{ fontSize: "30px", color: "rgb(18, 99, 241)" }}>
+              <span>APR </span>
+              <span>2000%</span>
+            </h5>
+            <p className="card-text">Earn passive income with our staking reward.</p>
+            <a href="#" className="btn btn-primary stake-btn">Unlock Wallet</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  )
+}
